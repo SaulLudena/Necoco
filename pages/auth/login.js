a2c101a9b9bfd6a50d4e346149aa62e754ff406d
SaulLudena <cornejo758@gmail.com>
2023-09-29 23:21:34 -0500

    updating eveything

---
+++ pages/auth/login.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Header from "../../src/components/header";
+import Recover_password from "../../src/components/recoverPassword";
+export default function RecoverPassword() {
+  return (
+    <>
+      <Head>
+        <title>Iniciar sesión</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="./logo.png" />
+      </Head>
+
+      <div className="h-screen  bg-[#1e1e1e]">
+        <Header />
+
+        <div className="grid grid-cols-12  h-[88%] ">
+          venta de inicio de sesión
+        </div>
+      </div>
+    </>
+  );
+}
